fb8c692a92a2e7086f2814fcaea9b3210caf62c4
hemantCC <49364193+hemantCC@users.noreply.github.com>
2020-10-17 16:52:12 +0530

    Update add-customer.component.spec.ts

--- src/app/components/add-customer/add-customer.component.spec.ts
+++ src/app/components/add-customer/add-customer.component.spec.ts
@@ -1,25 +1,56 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-// import { AddCustomerComponent } from './add-customer.component';
+import { ControlService } from 'src/app/services/control.service';
+import { AddCustomerComponent } from './add-customer.component';
 
-// describe('AddCustomerComponent', () => {
-//   let component: AddCustomerComponent;
-//   let fixture: ComponentFixture<AddCustomerComponent>;
+describe('AddCustomerComponent', () => {
+  let component: AddCustomerComponent;
+  let fixture: ComponentFixture<AddCustomerComponent>;
+  let controlService: ControlService;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ AddCustomerComponent ]
-//     })
-//     .compileComponents();
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddCustomerComponent ],
+      providers:[{
+          provide:ControlService, useValue:{ getControls: () => of([{
+           name: 'salutation',
+            entityName: 'Salutation' ,
+            isRequired:true,
+            controlType:2,Module:1,
+            dropdownValues:['value1','value2']}]) }
+        }]
+    })
+    .compileComponents();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(AddCustomerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddCustomerComponent);
+    component = fixture.componentInstance;
+    controlService = TestBed.get(ControlService);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('populateControls should be called',()=>{
+    spyOn(controlService,'getControls').and.callThrough();
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(controlService.getControls).toHaveBeenCalled();
+  })
+
+  it('populateControls should return controls',()=>{
+    spyOn(controlService,'getControls').and.callThrough();
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.controls).toEqual([{ name: 'salutation',
+    entityName: 'Salutation' ,
+    isRequired:true,
+    controlType:2,Module:1,
+    dropdownValues:['value1','value2']}]);
+  })
+
+})
