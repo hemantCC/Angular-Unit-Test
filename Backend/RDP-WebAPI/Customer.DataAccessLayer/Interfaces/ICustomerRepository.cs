c0199fc7bdeeddca06fd3a639e161370cd91a293
hemantCC <49364193+hemantCC@users.noreply.github.com>
2020-10-21 12:00:18 +0530

    api

---
+++ Backend/RDP-WebAPI/Customer.DataAccessLayer/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Customer.EntityLayer.Entities.BusinessEntities;
+using Customer.EntityLayer.Entities.DataEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.DataAccessLayer.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        TblCustomer AddCustomer(TblCustomer customer);
+        Task<bool> AddMaster1(TblMaster1 master1);
+        Task<bool> AddMaster2(TblMaster2 master2);
+        Task<bool> AddFAInformation(TblFainformation fainformation);
+        Task<bool> AddConsultant(TblConsultant consultant);
+        Task<CustomerVM> GetCustomerDetailsById(int id);
+        Task<List<CustomerDetailsVM>> GetCustomerDetailsAsync();
+    }
+}
